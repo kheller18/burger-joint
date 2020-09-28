496c6aa984774392dc2a19dca2744e66513cc7fc
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-09-27 21:05:29 -0700

    added heroku jawsdb connection route

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "6198HawkS6198!",
-    database: "burgersDB"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "6198HawkS6198!",
+        database: "burgersDB"
+    });    
+};
 
 connection.connect(function(err) {
     if (err) {
